2839424e9e1d69b9a3470a763b36063cc405b307
zhangbo56 <zhangbo56@xiaomi.com>
2025-10-27 14:31:16 +0800

    vela: Add vela initial mk file
    Signed-off-by: zhangbo56 <zhangbo56@xiaomi.com>
    Signed-off-by: zhangbo56 <zhangbo56@xiaomi.com>

---
+++ android/vela.mk
@@ -0,0 +1,27 @@
+ifneq ($(filter goldfish64_%, $(TARGET_DEVICE)),)
+PRODUCT_COPY_FILES += \
+    vendor/vela/build/android/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-xiaomi.txt
+endif
+
+PRODUCT_BOOT_JARS += cpc-extension
+
+PRODUCT_PACKAGES += \
+    kvget \
+    kvset \
+    rpsock_client \
+    rpsock_server \
+    TestServerCpc \
+    TestClientCpc \
+    cpc-extension \
+    libcpc_extension_jni.xiaomi
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/kvget \
+    system/bin/kvset \
+    system/bin/rpsock_client \
+    system/bin/rpsock_server \
+    system/bin/TestServerCpc \
+    system/bin/TestClientCpc \
+    system/framework/cpc-extension.jar \
+    system/lib64/libcpc_extension_jni.xiaomi.so \
+    system/etc/permissions/cpc-extension.xml
